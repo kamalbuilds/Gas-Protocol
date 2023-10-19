7d2fa81a7a4382dc92763d9d13280ab3225882a5
abhishek-01k <abhishek@push.org>
2023-10-20 01:43:13 +0530

    Function extractor working

--- src/app/Extractor.tsx
+++ src/app/Extractor.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { FormControl, FormLabel, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import FunctionForm from '@/components/ContractExtractor/FunctionForm';
+import { FormControl, FormLabel, Input, InputGroup, InputRightElement, Select } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { Oval } from 'react-loader-spinner'
 
@@ -9,6 +10,21 @@ const Extractor = () => {
     const [contractAddress, setContractAddress] = useState();
     const [contractABI, setContractABI] = useState({});
 
+    const [functions, setFunctions] = useState<any>({});
+
+    const [functionSelected, setFunctionSelected] = useState(null);
+
+    const [functionInput, setFunctionInput] = useState(null);
+
+    console.log("Functions", functions);
+
+    const handleSelect = (e) => {
+        setFunctionSelected(e.target.value);
+        setFunctionInput(functions[e.target.value]?.inputs);
+    }
+
+    const [isLoading, setIsLoading] = useState(false);
+
     const handleInputChange = (e: any) => {
         e.preventDefault();
         console.log("Change", e.target.value);
@@ -16,6 +32,7 @@ const Extractor = () => {
     }
 
     const handleContractAddress = async () => {
+        setIsLoading(true);
         console.log("Contract Address", contractAddress);
 
         const ETHERSCAN_API = process.env.ETHERSCAN_API_KEY;
@@ -27,33 +44,53 @@ const Extractor = () => {
                 const res = await fetch(url);
                 const response = await res.json();
 
-                console.log("Response", response);
-                setContractABI(response);
+                const abiresponse = JSON.parse(response.result);
+                setContractABI(abiresponse);
+
+                getFunctionsFromABI(abiresponse);
+                setIsLoading(false);
             } catch (error) {
                 console.log("Error", error);
+                setIsLoading(false);
             }
         }
     }
 
-    const getFunctionsFromABI = () => {
-        if (contractABI) {
-            console.log("Contract ABI", contractABI);
+    const getFunctionsFromABI = (abiresponse: any) => {
+        if ((Object.keys(abiresponse).length !== 0)) {
+            console.log("Contract ABI", abiresponse);
+            const functionList = abiresponse.filter((element: any) => element.type === 'function' && element.stateMutability !== 'view');
 
+            functionList?.map((item: any) => {
+                console.log("Item", item, typeof (item));
+
+                setFunctions((functions: any) => ({
+                    ...functions,
+                    [item.name]: { ...item }
+                }));
+
+            })
 
         }
     }
+
+
+
+    const handleFunctionInput = (e) => {
+        console.log("Input data", e.target.value);
+    }
+
+    console.log("Function input", functionInput);
 
     return (
         <div className='w-9/12'>
             hey
 
-            <div>
-                <p>Enter the Contract Details</p>
-
-                <FormControl >
+            <div className='flex flex-row'>
+                <FormControl className='flex-1'>
                     <FormLabel>Enter Contract Address</FormLabel>
                     <InputGroup>
-                        <Input placeholder='Enter amount' onChange={handleInputChange} />
+                        <Input placeholder='Enter Contract Address' onChange={handleInputChange} />
                         <InputRightElement>
                             <Oval
                                 height={25}
@@ -61,7 +98,7 @@ const Extractor = () => {
                                 color="#4fa94d"
                                 wrapperStyle={{}}
                                 wrapperClass=""
-                                visible={true}
+                                visible={isLoading ? true : false}
                                 ariaLabel='oval-loading'
                                 secondaryColor="#4fa94d"
                                 strokeWidth={2}
@@ -71,10 +108,52 @@ const Extractor = () => {
                     </InputGroup>
 
                     <button className='border-2 p-4' onClick={handleContractAddress}> Submit </button>
-                    <button className='border-2 p-4' onClick={getFunctionsFromABI}> Get Functions </button>
 
-                    {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+                    {Object.keys(functions).length !== 0 && (
+                        <FunctionForm
+                            functions={functions}
+                            contractAddress={contractAddress}
+                            handleInputChange={handleInputChange}
+                            handleSelect={handleSelect}
+                            functionInput={functionInput}
+                            handleFunctionInput={handleFunctionInput}
+                        />
+                    )}
+
                 </FormControl>
+
+                <div className='flex-1'>
+                    <div className='flex flex-col '>
+                        <div className='text-center'>Your transaction Batch</div>
+                        <div>
+                            <p className='text-[18px]'>&#123;</p>
+                            <div>
+                                <div className='flex flex-row gap-2'>
+                                    <p>To: </p>
+                                    <p>{contractAddress}</p>
+                                </div>
+                                <div className='flex flex-row gap-2'>
+                                    <p>Function: </p>
+                                    <p>{functionSelected}</p>
+                                </div>
+
+                                {functionInput && (
+                                    functionInput?.map((item, index) => {
+                                        console.log("Item", item);
+                                        return (
+                                            <div key={index}>{item?.name}</div>
+                                        )
+                                    })
+                                )}
+
+                                <div className='flex flex-row gap-2'>
+                                    <p></p>
+                                </div>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
---
+++ src/components/ContractExtractor/FunctionForm.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { FormControl, FormLabel, Input, Select } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+const FunctionForm = ({
+    functions,
+    contractAddress,
+    handleInputChange,
+    handleSelect,
+    functionInput,
+    handleFunctionInput
+}: any) => {
+
+    console.log("Functions", functions);
+
+
+    return (
+        <div>
+            <FormControl>
+
+                <FormLabel>To Address</FormLabel>
+                <Input value={contractAddress} onChange={handleInputChange} />
+
+                <FormLabel>Contract Function Selection</FormLabel>
+                <Select placeholder='Select Function to Call' onChange={handleSelect}>
+                    {Object.keys(functions).map((item, index) => (
+                        <option key={index}>{item}</option>
+                    ))}
+                </Select>
+
+                {functionInput && (
+                    functionInput.map((item, index) => (
+                        <div key={index}>
+                            <FormLabel>{item?.name} ({item?.type})</FormLabel>
+                            <Input placeholder={`Enter ${item?.name}`} onChange={handleFunctionInput} />
+                        </div>
+                    ))
+                )}
+
+            </FormControl>
+        </div>
+    );
+};
+
+export default FunctionForm;
